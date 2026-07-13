e08a190a02ceb38891e4c523cd286144836f582a
Ragheed <ragheed@muhide.ai>
2026-07-13 17:24:13 +0300

    chore: remove tmp_test.py, update conftest

--- salesos/backend/tmp_test.py
+++
@@ -1,4 +0,0 @@
-﻿from domains.search.normalization.arabic_normalizer import ArabicSearchNormalizer
-n = ArabicSearchNormalizer.default()
-print("sukun:", repr(n.normalize('\u0645\u0650\u0646\u0652')))
-print("conj:", repr(n.normalize('\u0634\u0631\u0643\u0629 \u0648 \u0645\u0624\u0633\u0633\u0629')))
